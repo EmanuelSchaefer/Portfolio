c5b4fcbc9995db077cd28840c026aa73cf555a17
Emanuel Schäfer <emanuel-schaefer1@web.de>
2024-03-11 19:08:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PotfolioComponent } from './potfolio/potfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+    HeaderComponent,
+    LandingPageComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PotfolioComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PotfolioComponent } from './potfolio/potfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeaderComponent },
+    { path: '', component: LandingPageComponent },
+    { path: '', component: AboutMeComponent },
+    { path: '', component: MySkillsComponent },
+    { path: '', component: PotfolioComponent },
+    { path: '', component: ContactComponent },
+    { path: '', component: FooterComponent }
+];
